e10e6cfdf01b82d9dbd575ca8a40ba557006754b
khitamdao <50797537+khitamdao@users.noreply.github.com>
2020-12-29 10:57:35 +0700

    Update nhaccuatui.js

--- DichVuTrucTuyen/nhaccuatui.js
+++ DichVuTrucTuyen/nhaccuatui.js
@@ -3,7 +3,7 @@ let url = $request.url;
 var cons = "users/info";
 if(url.indexOf(cons) != -1)
 {
-obj.data.VIPExpire= "27/03/9999 00:00:00";
+obj.data.VIPExpire= "09/09/2099 00:00:00";
 obj.data.isVIP= true;
 }
 $done({body: JSON.stringify(obj)});
