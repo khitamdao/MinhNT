369e61cc88fea4afde34768125308a4d40e65b42
khitamdao <50797537+khitamdao@users.noreply.github.com>
2021-06-04 15:34:26 +0700

    Update Pdfexpert.vip.js

--- Documents/Pdfexpert.vip.js
+++ Documents/Pdfexpert.vip.js
@@ -1,41 +1,19 @@
-var obj= {
-  "productId": "com.readdle.PDFExpert5.subscription.year50_pe6",
-  "subscriptionExpirationIntent": "userCancelled",
-  "receiptStatus": "ok",
-  "subscriptionExpirationDate": "13:15 03/11/2099",
-  "isPDFExpert6User": false,
-  "inAppStates": [
-    {
-      "productId": "com.readdle.PDFExpert5.subscription.year50_pe6",
-      "subscriptionExpirationIntent": "userCancelled",
-      "receiptStatus": "ok",
-      "subscriptionExpirationDate": "13:15 03/11/2099",
-      "isEligibleForIntroPeriod": false,
-      "originalTransactionId": "20000618444996",
-      "productName": "subscription",
-      "isInBillingRetryPeriod": false,
-      "type": "subscription",
-      "subscriptionState": "active",
-      "subscriptionAutoRenewStatus": "autoRenewOff",
-      "isInGracePeriod": false
-    }
-  ],
-  "isEligibleForIntroPeriod": false,
-  "originalTransactionId": "20000618444996",
-  "isEligibleFor": [
-    "winback"
-  ],
-  "isInBillingRetryPeriod": false,
-  "type": "subscription",
-  "inAppPurchased": [
-    "com.readdle.PDFExpert5.subscription.year50_pe6"
-  ],
-  "bundleId": "com.readdle.PDFExpert5",
-  "receiptId": 1572178404000,
-  "chargingPlatform": "iOS AppStore",
-  "subscriptionState": "active",
-  "subscriptionAutoRenewStatus": "autoRenewOff",
-  "isInGracePeriod": false
-};
 
-$done({body: JSON.stringify(obj)});
+re('\\w{1,2}\/\\w{1,2}\/\\w{4}@"subscriptionState":"\\w+"@"is(.+)User":false','25/11/2099@"subscriptionState":"active"@"is$1User":true');
+
+function re() {
+var body = $response.body;
+ if (arguments[0].includes("@")) {
+  var regs = arguments[0].split("@");
+  var strs = arguments[1].split("@");
+  for (i = 0;i < regs.length;i++) {
+   var reg = new RegExp(regs[i],"g");
+   body = body.replace(reg, strs[i]);
+ }
+}
+ else {
+  var reg = new RegExp(arguments[0],"g");
+  body = body.replace(reg, arguments[1]);
+}
+ $done({body});
+} 
