87d95f6077829243b141c2a6363da4202a5d65d9
khitamdao <50797537+khitamdao@users.noreply.github.com>
2021-06-23 12:11:37 +0700

    Update zingmp3.js

--- DichVuTrucTuyen/zingmp3.js
+++ DichVuTrucTuyen/zingmp3.js
@@ -1,15 +1,14 @@
 let obj= JSON.parse($response.body);
 
-if($response.body.indexOf("lastVipExpireTime") !=-1){
+
 delete obj.data.lastVipExpireTime;
-}
 obj.data.vip= {
-  "expireTime": 4096412384000,
-  "startTime": 1572527803000,
+  "expireTime": 1892800999,
+  "startTime": 1572527803,
   "subscription": {
     "status": 0,
-    "expireTime": 4096412384000,
-    "platform": 5
+    "expireTime": 1892800999,
+    "platform": 2
   },
   "vipType": 1
 }
