6f42ac91d0de666d07296f5370659c35e80dd135
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-06-05 22:52:55 +0700

    Update zingmp3.js

--- Script/zingmp3.js
+++ Script/zingmp3.js
@@ -1,17 +1,5 @@
-let obj= JSON.parse($response.body);
-
-if($response.body.indexOf("lastVipExpireTime") !=-1){
-delete obj.data.lastVipExpireTime;
-}
-obj.data.vip= {
-  "expireTime": 4096412384000,
-  "startTime": 1572527803000,
-  "subscription": {
-    "status": 0,
-    "expireTime": 4096412384000,
-    "platform": 5
-  },
-  "vipType": 1
-}
-
-$done({body:JSON.stringify(obj)});
+body = $response.body.replace(/"lastVipExpireTime".*\d{10}\n/, "");
+let obj = JSON.parse(body);
+obj["data"]["sessionsKey"]= "sm1.mfc_.1009719336.10.vjnZ26bVD1L1HnifQLyq5MbVD1LfNpGLQDphhpHVD1K";
+obj["data"]["vip"]= {"expireTime":1869254270,"startTime":1572527803,"subscription":{"status":1,"expireTime": 1869254270,"platform":2},"vipType":1};
+$done({body: JSON.stringify(obj)});
