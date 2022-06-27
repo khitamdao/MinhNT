6dd789c4c8c63a024b3efc92d4dca6793fc1dcbe
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-06-28 06:15:34 +0700

    Create WPSOffice.js

---
+++ Script/WPSOffice.js
@@ -0,0 +1,55 @@
+/*
+WPS Office 解锁部分功能
+
+***************************
+QuantumultX:
+
+[rewrite_local]
+^https?:\/\/account\.wps\.cn\/api\/users url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Wps.js
+
+[mitm]
+hostname = account.wps.cn
+
+***************************
+Surge4 or Loon:
+
+[Script]
+http-response ^https?:\/\/account\.wps\.cn\/api\/users requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Wps.js
+
+[MITM]
+hostname = account.wps.cn
+
+**************************/
+
+var body = JSON.parse($response.body);
+var obj = {
+  exp: 0,
+  level: 3,
+  privilege: [
+    { spid: "data_recover", times: 0, expire_time: 1846256142 },
+    { spid: "ocr", times: 0, expire_time: 1846256142 },
+    { spid: "pdf2doc", times: 0, expire_time: 1846256142 },
+    { spid: "pdf_merge", times: 0, expire_time: 1846256142 },
+    { spid: "pdf_sign", times: 0, expire_time: 1846256142 },
+    { spid: "pdf_split", times: 0, expire_time: 1846256142 }
+  ],
+  result: "ok",
+  total_buy: 0,
+  total_cost: -30,
+  userid: body.userid,
+  vip: {
+    name: "WPS VIP Yearly",
+    has_ad: 0,
+    memberid: 40,
+    expire_time: 1846256142,
+    enabled: [
+      { memberid: 40, name: "WPS VIP Yearly", expire_time: 1846256142 },
+      { memberid: 20, name: "WPS VIP Quarterly", expire_time: 1846256142 },
+      { memberid: 12, name: "WPS VIP Monthly", expire_time: 1846256142 }
+    ]
+  },
+  wealth: 0,
+  expire_time: 1846256142
+};
+
+$done({ body: JSON.stringify(obj) });
