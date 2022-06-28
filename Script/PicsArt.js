452280977ae5c3a909b9ccdfae9dadf43cc2c3c4
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-06-28 08:35:10 +0700

    Update PicsArt.js

--- Script/PicsArt.js
+++ Script/PicsArt.js
@@ -4,7 +4,7 @@
 QuantumultX:
 
 [rewrite_local]
-^https:\/\/api\.(picsart|meiease)\.c(n|om)\/users\/show\/me\.json url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/PicsArt.js
+^https:\/\/api\.(picsart|meiease)\.c(n|om)\/users\/show\/me\.json url script-response-body https://raw.githubusercontent.com/khitamdao/MinhNT/master/Script/PicsArt.js
 
 [mitm]
 hostname = api.picsart.c*, api.meiease.c*
@@ -13,7 +13,7 @@ hostname = api.picsart.c*, api.meiease.c*
 Surge4 or Loon:
 
 [Script]
-http-response https:\/\/api\.(picsart|meiease)\.c(n|om)\/users\/show\/me\.json requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/PicsArt.js
+http-response https:\/\/api\.(picsart|meiease)\.c(n|om)\/users\/show\/me\.json requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/khitamdao/MinhNT/master/Script/PicsArt.js
 
 [MITM]
 hostname = api.picsart.c*, api.meiease.c*
