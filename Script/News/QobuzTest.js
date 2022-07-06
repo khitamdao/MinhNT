d6a95cbc62c622f4ec61f015d2bdf47e962d3d2c
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-06 15:02:09 +0700

    Update QobuzTest.js

--- Script/News/QobuzTest.js
+++ Script/News/QobuzTest.js
@@ -2,7 +2,7 @@
 
 [rewrite_local]
 # > Qobuz解锁设备限制测试
-^https?:\/\/www\.qobuz\.com\/api\.json\/0\.2\/file\/url url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/QobuzTest.js
+^https?:\/\/www\.qobuz\.com\/api\.json\/0\.2\/file\/url url script-request-header https://raw.githubusercontent.com/khitamdao/MinhNT/master/Script/News/QobuzTest.js
 
 [mitm] 
 hostname = www.qobuz.com
