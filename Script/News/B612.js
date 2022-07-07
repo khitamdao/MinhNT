b36808940c9801ea6e1d4715c90835e69f6d96a0
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-07 15:59:46 +0700

    Update B612.js

--- Script/News/B612.js
+++ Script/News/B612.js
@@ -3,7 +3,7 @@
     "products" : [
       {
         "managed" : false,
-        "status" : "OK",
+        "status" : "CANCELLED",
         "startDate" : 1657183415000,
         "productId" : "com.linecorp.b612.vip.oneyear",
         "expireDate" : 4073275762000
