846935f69554e62a454468bbabb3504b7b0d7afd
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-07 21:52:18 +0700

    Update Ulike.js

--- Script/News/Ulike.js
+++ Script/News/Ulike.js
@@ -1,4 +1,5 @@
 {
+  "sign" : "b748RemyovExTJk9M8/fT1oT+TDJfu2Qv0O/36n/AuxnxU0kqzlELZQDhFcmCbZuOHN8udzVqJm9kDHGT4SMzP/Txd5UtVm12jVfe0NnFPVzEzUUROBnyFL3DO0k++BF/W5QAzfjpuLzMcvS904vBN8QGGf7yans+aaiRb3k7RY=",
   "data" : {
     "cycle_unit" : "MONTH",
     "uid" : "0",
@@ -13,5 +14,9 @@
     "space_cap" : 0,
     "end_time" : 6619406400,
     "is_first_subscribe" : false
-  }
+  },
+  "response" : "{\"flag\":true,\"start_time\":1656580777,\"end_time\":6619406400,\"is_first_subscribe\":false,\"is_cancel_subscribe\":false,\"uid\":\"0\",\"subscribe_uid\":\"7114957930496165378\",\"subscribe_type\":\"auto\",\"authkey\":\"42d7d6efce7244017a4ca6fc8f6ceb64\",\"product_id\":\"beauty_subscription_year\",\"subscribe_cycle\":12,\"cycle_unit\":\"MONTH\",\"space_cap\":0}",
+  "systime" : "1657176965562",
+  "errmsg" : "success",
+  "ret" : "0"
 }
