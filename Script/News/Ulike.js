ef0827f558554b50951fbfc9b4d486f41e8c7407
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-07 16:18:26 +0700

    Update Ulike.js

--- Script/News/Ulike.js
+++ Script/News/Ulike.js
@@ -12,10 +12,10 @@
     "subscribe_uid" : "7114957930496165378",
     "product_id" : "beauty_subscription_year",
     "space_cap" : 0,
-    "end_time" : 4073275762,
+    "end_time" : 6619406400,
     "is_first_subscribe" : false
   },
-  "response" : "{\"flag\":true,\"start_time\":1656580777,\"end_time\":1657185585,\"is_first_subscribe\":false,\"is_cancel_subscribe\":false,\"uid\":\"0\",\"subscribe_uid\":\"7114957930496165378\",\"subscribe_type\":\"auto\",\"authkey\":\"42d7d6efce7244017a4ca6fc8f6ceb64\",\"product_id\":\"beauty_subscription_year\",\"subscribe_cycle\":12,\"cycle_unit\":\"MONTH\",\"space_cap\":0}",
+  "response" : "{\"flag\":true,\"start_time\":1656580777,\"end_time\":6619406400,\"is_first_subscribe\":false,\"is_cancel_subscribe\":false,\"uid\":\"0\",\"subscribe_uid\":\"7114957930496165378\",\"subscribe_type\":\"auto\",\"authkey\":\"42d7d6efce7244017a4ca6fc8f6ceb64\",\"product_id\":\"beauty_subscription_year\",\"subscribe_cycle\":12,\"cycle_unit\":\"MONTH\",\"space_cap\":0}",
   "systime" : "1657176965562",
   "errmsg" : "success",
   "ret" : "0"
