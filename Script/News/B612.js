7d1318abde21d2e5e772b9592ecb744743e84d16
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-07 17:41:07 +0700

    Update B612.js

--- Script/News/B612.js
+++ Script/News/B612.js
@@ -1,14 +1,1 @@
-{
-  "result" : {
-    "products" : [
-      {
-        "managed" : false,
-        "status" : "ACTIVE",
-        "startDate" : 1657187904000,
-        "productId" : "com.linecorp.b612.vip.oneyear",
-        "expireDate" : 6619406400000
-      }
-    ],
-    "activated" : true
-  }
-}
+{"result":{"activated":true,"products":[{"productId":"com.linecorp.b612.vip.oneyear","startDate":1657187904000,"expireDate":6619406400000,"managed":false,"status":"ACTIVE"}]}}
