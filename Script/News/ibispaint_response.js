bb6a9e0181371252079b1ffd7b87ea3bf3bee2f2
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-08 14:24:38 +0700

    Create ibispaint_response.js

---
+++ Script/News/ibispaint_response.js
@@ -0,0 +1,1 @@
+{"result":1,"purchases":[{"cancel_date":null,"cancel_reason":0,"first_purchase_date":1657263798000,"first_purchase_id":"490001108492104","item":5,"last_expire_date":6619406400000,"last_purchase_date":1657263797000,"last_purchase_id":"490001108492104","next_purchase_id":"ibisPaintXPrimeYearly","renew_state":true,"test_flag":false,"token":null,"trial_mode":true}]}
