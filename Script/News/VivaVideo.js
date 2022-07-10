35d4f211722df73faffaa801f239cc1ddc9c8c61
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-10 22:27:43 +0700

    Update VivaVideo.js

--- Script/News/VivaVideo.js
+++ Script/News/VivaVideo.js
@@ -5,7 +5,7 @@
 QuantumultX:
 
 [rewrite_local]
-^https:\/\/(xy-viva\.kakalili|api-chn.rthdo)\.com\/api\/rest\/u\/vipVerifyReceipt url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/vivavideo.js
+^https:\/\/(xy-viva\.kakalili|api-chn.rthdo)\.com\/api\/rest\/u\/vipVerifyReceipt url script-response-body https://raw.githubusercontent.com/khitamdao/MinhNT/master/Script/News/VivaVideo.js
 
 [mitm]
 hostname = xy-viva.kakalili.com, api-chn.rthdo.com
@@ -14,7 +14,7 @@ hostname = xy-viva.kakalili.com, api-chn.rthdo.com
 Surge4 or Loon:
 
 [Script]
-http-response ^https:\/\/(xy-viva\.kakalili|api-chn.rthdo)\.com\/api\/rest\/u\/vipVerifyReceipt requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/vivavideo.js
+http-response ^https:\/\/(xy-viva\.kakalili|api-chn.rthdo)\.com\/api\/rest\/u\/vipVerifyReceipt requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/khitamdao/MinhNT/master/Script/News/VivaVideo.js
 
 [MITM]
 hostname = xy-viva.kakalili.com, api-chn.rthdo.com
