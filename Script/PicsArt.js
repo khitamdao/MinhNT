c490d7848d2b6a3fbf836e92d649df1ebfa1b052
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-14 23:45:56 +0700

    Update PicsArt.js

--- Script/PicsArt.js
+++ Script/PicsArt.js
@@ -1,25 +1,3 @@
-/**** PicsArt ***
-
-***************************
-QuantumultX:
-
-[rewrite_local]
-^https:\/\/api\.(picsart|meiease)\.c(n|om)\/users\/show\/me\.json url script-response-body https://raw.githubusercontent.com/khitamdao/MinhNT/master/Script/PicsArt.js
-
-[mitm]
-hostname = api.picsart.c*, api.meiease.c*
-
-***************************
-Surge4 or Loon:
-
-[Script]
-http-response https:\/\/api\.(picsart|meiease)\.c(n|om)\/users\/show\/me\.json requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/khitamdao/MinhNT/master/Script/PicsArt.js
-
-[MITM]
-hostname = api.picsart.c*, api.meiease.c*
-
-**************************/
-
 let obj = JSON.parse($response.body);
 obj.subscription.granted = "true";
 $done({body: JSON.stringify(obj)});
