4700560483b4aa63e442b0ba5857290ea818cbbc
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-25 20:15:48 +0700

    Update ZingMp3.js

--- Script/News/ZingMp3.js
+++ Script/News/ZingMp3.js
@@ -1,15 +1,14 @@
-let obj= JSON.parse($response.body);
-
-delete obj.data.lastVipExpireTime;
-obj.data.vip= {
-  "expireTime": 6619406400,
-  "startTime": 1572527803,
-  "subscription": {
-    "status": 0,
-    "expireTime": 6619406400,
-    "platform": 2
-  },
-  "vipType": 1
+{
+  "err" : 0,
+  "sTime" : 1658754043293,
+  "eTime" : 6619406400000,
+  "data" : {
+    "createdTime" : 1653307488306,
+    "subscription" : [
+      "status": 0,
+      "expireTime": 6619406400000,
+      "platform": 2
+    ],
+    "vipType": 1
+    }
 }
-
-$done({body:JSON.stringify(obj)});
