625f09ef93ebb1804b230e4f358a82170ad4b424
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-26 18:41:42 +0700

    Update ZingMp3.js

--- Script/News/ZingMp3.js
+++ Script/News/ZingMp3.js
@@ -1,8 +1,15 @@
+const avatar = JSON.parse($response.body["avatar"].value);
+const name = JSON.parse($response.body["name"].value);
+
 let obj = JSON.parse($response.body);
 obj = {
-  "data":{
+  "err" : 0,
+  "sTime" : 1658760867433,
+  "data" : {
+    "id" : 1085568223,
     "createdTime" : 1653307488306,
     "hasVipTrial" : false,
+    "boolAtt" : 18,
     "vip" : {
       "startTime" : 1658760867403,
       "vipType" : 1,
@@ -13,6 +20,24 @@ obj = {
       },
       "expireTime" : 6619406400000
     },
+    "csuId" : "cs1.67d1800f080ae154b81b",
+    "obStatus" : 1,
+    "vipObStatus" : 0,
+    "segments" : "suggest-playlist-default",
+    "ipCountry" : "VN",
+    "birthdate" : "",
+    "euId" : "1a11fdcf75ca9c94c5db",
+    "avatar" : avatar,
+    "facebookId" : "",
+    "vipBanner" : 0,
+    "upload" : {
+      "status" : 1,
+      "remainSlot" : 1000,
+      "totalSlot" : 1000
+    },
+    "name" : name,
+    "gender" : 0
   },
+  "msg" : "Success"
 }
 $done({body: JSON.stringify(obj)});
