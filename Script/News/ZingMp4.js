ace9005a3200af30d2bcfa853548f219c7344a2c
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-26 19:49:50 +0700

    Update ZingMp4.js

--- Script/News/ZingMp4.js
+++ Script/News/ZingMp4.js
@@ -5,16 +5,13 @@ Update: 26/07/2022
 */
 const avatar = JSON.parse($response.body)["data"]["avatar"];
 const name = JSON.parse($response.body)["data"]["name"];
-const id = JSON.parse($response.body)["data"]["id"];
-/* const csuId = JSON.parse($response.body)["data"]["csuId"]; */
-const euId = JSON.parse($response.body)["data"]["euId"];
 
 let obj = JSON.parse($response.body);
 obj = {
   "err" : 0,
   "sTime" : 1658760867433,
   "data" : {
-    "id" : id,
+    "id" : 1085568223,
     "createdTime" : 1653307488306,
     "hasVipTrial" : false,
     "boolAtt" : 18,
@@ -34,7 +31,7 @@ obj = {
     "segments" : "suggest-playlist-default",
     "ipCountry" : "VN",
     "birthdate" : "",
-    "euId" : euId,
+    "euId" : "1a11fdcf75ca9c94c5db",
     "avatar" : avatar,
     "facebookId" : "",
     "vipBanner" : 0,
