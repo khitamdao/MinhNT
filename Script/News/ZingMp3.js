8faf39780940870c86831bca703f50a7f3138eaf
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-27 06:10:38 +0700

    Create ZingMp3.js

---
+++ Script/News/ZingMp3.js
@@ -0,0 +1,31 @@
+/*
+Unlock Full version zing
+Nguyễn Trường Minh
+Update: 26/07/2022
+*/
+const avatar = JSON.parse($response.body)["data"]["avatar"];
+const name = JSON.parse($response.body)["data"]["name"];
+
+let obj = JSON.parse($response.body);
+obj = {
+  "sTime" : 1658760867433,
+  "data" : {
+    "id" : 1085568223,
+    "createdTime" : 1653307488306,
+    "vip" : {
+      "startTime" : 1658760867403,
+      "vipType" : 1,
+      "subscription" : {
+        "status" : 1,
+        "expireTime" : 6619406400000,
+        "platform" : 2
+      },
+      "expireTime" : 6619406400000
+    },
+    "csuId" : "cs1.67d1800f080ae154b81b",
+    "euId" : "1a11fdcf75ca9c94c5db",
+    "avatar" : avatar,
+    "name" : name,
+  },
+}
+$done({body: JSON.stringify(obj)});
