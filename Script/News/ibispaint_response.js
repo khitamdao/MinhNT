14ce6bfec86d1d051a1681b669e45659d96e6555
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-26 21:53:30 +0700

    Update ibispaint_response.js

--- Script/News/ibispaint_response.js
+++ Script/News/ibispaint_response.js
@@ -1,5 +1,22 @@
 let obj = JSON.parse($response.body);
 obj = {
-  "result":1,"purchases":[{"cancel_date":null,"cancel_reason":0,"first_purchase_date":1657263798000,"first_purchase_id":"490001108492104","item":5,"last_expire_date":6619406400000,"last_purchase_date":1657263797000,"last_purchase_id":"490001108492104","next_purchase_id":"ibisPaintXPrimeYearly","renew_state":true,"test_flag":false,"token":null,"trial_mode":true}]
+  "result" : 1,
+  "purchases" : [
+    {
+      "last_expire_date" : 6619406400000,
+      "cancel_date" : null,
+      "next_purchase_id" : "ibisPaintXPrimeYearly",
+      "renew_state" : true,
+      "item" : 5,
+      "test_flag" : false,
+      "last_purchase_id" : "490001108492104",
+      "token" : null,
+      "first_purchase_date" : 1657263798000,
+      "cancel_reason" : 0,
+      "trial_mode" : true,
+      "last_purchase_date" : 1657263797000,
+      "first_purchase_id" : "490001108492104"
+    }
+  ]
 }
 $done({body: JSON.stringify(obj)});
