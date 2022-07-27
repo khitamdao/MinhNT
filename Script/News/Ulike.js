cdd74652f006a54d20fca0f9b13227cb058756ce
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-28 00:56:10 +0700

    Update Ulike.js

--- Script/News/Ulike.js
+++ Script/News/Ulike.js
@@ -1,18 +1,27 @@
 const path1 = "/commerce/v1/subscription/user_info";
 const path2 = "/apple/iosAppVerifyReceipt.php";
 
-let obj = JSON.parse($response.body);
+let ddgksf2013 = JSON.parse($response.body);
 
-if ($request.url.indexOf(path1) != -1){
-obj.data.start_time = 1657263798;
-obj.data.end_time = 6619406400;
-obj.data.is_cancel_subscribe = true;
-obj.data.flag = true;
+if ($request.url.indexOf(path1) != -1)
+{
+     if(ddgksf2013.response){
+           var tmp = JSON.parse(ddgksf2013.response);
+           tmp.start_time = 1584674770;
+           tmp.end_time = 6619406400;
+           tmp.is_cancel_subscribe = false;
+           tmp.subscribe_type = "auto";
+           tmp.flag = true;
+           ddgksf2013.response =  JSON.stringify(tmp);  
+     }
+    ddgksf2013.data.start_time = 1584674770;
+    ddgksf2013.data.end_time = 6619406400;
+    ddgksf2013.data.is_cancel_subscribe = false;
+    ddgksf2013.data.subscribe_type = "auto";
+    ddgksf2013.data.flag = true;
 }
-if ($request.url.indexOf(path2) != -1){
- obj.data = {
-    "isValid": 1,
-    "expiresTs": 6619406400
+if ($request.url.indexOf(path2) != -1)
+{
+   ddgksf2013.data = {"isValid": 1,"expiresTs": 6619406400}
 }
-}
-$done({body: JSON.stringify(obj)});
+$done({body: JSON.stringify(ddgksf2013)});
