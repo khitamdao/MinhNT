1743848436e3d571743f67355aff8d68b32de6c1
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-30 23:33:39 +0700

    Update BeautyPlus.js

--- Script/News/BeautyPlus.js
+++ Script/News/BeautyPlus.js
@@ -6,6 +6,6 @@ Create by @khitamdao 30/07/2022
 
 let obj = JSON.parse($response.body);
 obj = {
-  "vip_expires_date" : "2099-12-31"
+  "vip_expires_date" : 6619406400000
 }
 $done({body: JSON.stringify(obj)});
