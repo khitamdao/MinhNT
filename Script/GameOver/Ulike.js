8f0029e6ed58ba33bbd9b14f19fd186b7a6d9017
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-30 22:05:13 +0700

    Update Ulike.js

--- Script/GameOver/Ulike.js
+++ Script/GameOver/Ulike.js
@@ -25,3 +25,26 @@ if ($request.url.indexOf(path2) != -1)
    ddgksf2013.data = {"isValid": 1,"expiresTs": 6619406400}
 }
 $done({body: JSON.stringify(ddgksf2013)});
+
+/***********************************
+> 應用名稱：醒图&Ulike&轻颜相机&vaporcam
+> 軟件版本：0.0.0
+> 下載地址：https://apps.apple.com/us/app
+> 腳本作者：Cuttlefish
+> 微信賬號：墨魚手記
+> 解鎖說明：解鎖高級會員權限
+> 更新時間：2022-07-25
+> 通知頻道：https://t.me/ddgksf2021
+> 問題反饋：https://t.me/ddgksf2013_bot
+> 特別說明：⛔⛔⛔
+           本腳本僅供學習交流使用，禁止轉載售賣
+           ⛔⛔⛔
+[rewrite_local]
+  
+# ～ 醒图&Ulike&轻颜相机&vaporcam（四合一）解鎖會員權限（2022-05-09）@ddgksf2013
+https://(commerce-.*api|pay).(faceu|wecut).(com|mobi)/(commerce|apple)/(iosAppVerifyReceipt.php|v1/subscription/user_info) url script-response-body https://github.com/ddgksf2013/Cuttlefish/raw/master/Crack/commerce.js
+
+[mitm] 
+hostname=commerce-i18n-api.faceu.mobi,commerce-api.faceu.mobi, pay.wecut.com
+
+***********************************/
