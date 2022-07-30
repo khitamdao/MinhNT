139ff97f970f3b6f7e438f0c00484446fdc05bd2
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-30 11:51:16 +0700

    Update ibispaint_response.js

--- Script/GameOver/ibispaint_response.js
+++ Script/GameOver/ibispaint_response.js
@@ -5,15 +5,15 @@ obj = {
     {
       "last_expire_date" : 6619406400000,
       "cancel_date" : null,
-      "purchase_id" : "ibisPaintXPrimeYearly",
-      "renew_state" : true,
+      "next_purchase_id" : "ibisPaintXPrimeYearly",
+      "renew_state" : false,
       "item" : 5,
       "test_flag" : false,
       "last_purchase_id" : "490001108492104",
       "token" : null,
       "first_purchase_date" : 1657263798000,
       "cancel_reason" : 0,
-      "trial_mode" : false,
+      "trial_mode" : true,
       "last_purchase_date" : 1657263797000,
       "first_purchase_id" : "490001108492104"
     }
