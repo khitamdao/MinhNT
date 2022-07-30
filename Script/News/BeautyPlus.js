fdc095675f4c6bf9d73f34fa32dfff9a65014349
khitamdao <50797537+khitamdao@users.noreply.github.com>
2022-07-30 23:11:29 +0700

    Create BeautyPlus.js

---
+++ Script/News/BeautyPlus.js
@@ -0,0 +1,11 @@
+/*
+Unlock Full version Beauty Plus
+Nguyễn Trường Minh
+Create by @khitamdao 30/07/2022
+*/
+
+let obj = JSON.parse($response.body);
+obj = {
+  "vip_expires_date" : "2099-12-31 23:59:59"
+}
+$done({body: JSON.stringify(obj)});
