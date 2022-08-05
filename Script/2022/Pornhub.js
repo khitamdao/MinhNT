106a738389c424fc871b0b6c9b1bb8cc116332e9
Nguyễn Trường Minh <50797537+khitamdao@users.noreply.github.com>
2022-08-05 14:49:54 +0700

    Update Pornhub.js

--- Script/2022/Pornhub.js
+++ Script/2022/Pornhub.js
@@ -1,8 +1,11 @@
 /*
 Date: 28/05/2022 by @ddgksf2013
+
+hostname = *.pornhub.com,
+
 # PornHub 网页优化（网址：https://cn.pornhub.com）
-^https:\/\/(cn|www)\.pornhub\.com\/_xa\/ads url reject-dict
-^https?:/\/(cn|www)\.pornhub\.com(\/?$|\/([?]|view|video).*$) url script-response-body https://github.com/ddgksf2013/Cuttlefish/raw/master/Html/JS/pornhub.js
+^https?:\/\/(cn|www)\.pornhub\.com\/_xa\/ads url reject-dict
+^https?:\/\/(cn|www)\.pornhub\.com(\/?$|\/([?]|view|video).*$) url script-response-body https://raw.githubusercontent.com/khitamdao/MinhNT/master/Script/2022/Pornhub.js
 */
 
 var body = $response.body
