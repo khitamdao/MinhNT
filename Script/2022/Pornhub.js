03d5e88888cc17e5f92ad0917653857eae6dc34e
Nguyễn Trường Minh <50797537+khitamdao@users.noreply.github.com>
2022-08-05 14:47:05 +0700

    Update and rename Script/News/Pornhub.js to Script/2022/Pornhub.js

---
+++ Script/2022/Pornhub.js
@@ -0,0 +1,10 @@
+/*
+Date: 28/05/2022 by @ddgksf2013
+# PornHub 网页优化（网址：https://cn.pornhub.com）
+^https:\/\/(cn|www)\.pornhub\.com\/_xa\/ads url reject-dict
+^https?:/\/(cn|www)\.pornhub\.com(\/?$|\/([?]|view|video).*$) url script-response-body https://github.com/ddgksf2013/Cuttlefish/raw/master/Html/JS/pornhub.js
+*/
+
+var body = $response.body
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Cuttlefish@v0.3/Html/CSS/pornhub.css" type="text/css">');
+$done({ body });
