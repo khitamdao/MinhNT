cb661eca58db06d93bea0ac1a4b27b47f0206fdb
Nguyễn Trường Minh <50797537+khitamdao@users.noreply.github.com>
2022-10-28 14:14:57 +0700

    Update Ulike.js

--- Script/GameOver/Ulike.js
+++ Script/GameOver/Ulike.js
@@ -1,5 +1,9 @@
 /* ulike VIP (By @songyangzz)
 
+QuaX
+^https:\/\/commerce-.*api\.faceu\.mobi\/commerce\/v1\/subscription\/user_info$ url script-response-body https://raw.githubusercontent.com/khitamdao/MinhNT/master/Script/GameOver/Ulike.js
+
+Surge
 ^https:\/\/commerce-.*api\.faceu\.mobi\/commerce\/v1\/subscription\/user_info$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/khitamdao/MinhNT/master/Script/GameOver/Ulike.js,script-update-interval=0
 
 hostname:commerce-i18n-api.faceu.mobi,commerce-api.faceu.mobi
