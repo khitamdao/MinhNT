7f6194562f7e6f2751ef3350d093c18c79ed6e27
Nguyễn Trường Minh <50797537+khitamdao@users.noreply.github.com>
2024-08-06 03:33:32 +0700

    Update Adobe.js

--- Scripts/Adobe.js
+++ Scripts/Adobe.js
@@ -1,9 +1,26 @@
-﻿var body = $response.body;
+var body = $response.body;
 var obj = JSON.parse(body);
 
-obj.mobileProfile.profileStatus = "PROFILE_AVAILABLE",
-obj.mobileProfile.legacyProfile = "{\"licenseId\":\"\",\"licenseType\":3,\"licenseVersion\":\"1.0\",\"effectiveEndTimestamp\":6619406400000,\"graceTime\":8553600000,\"licensedFeatures\":[],\"enigmaData\":{\"productId\":598,\"serialKey\":\"\",\"clearSerialKey\":\"\",\"locale\":\"ALL\",\"associatedLocales\":\"ALL\",\"platform\":0,\"isk\":5984027,\"customerId\":0,\"deliveryMethod\":3,\"pc\":true,\"rb\":true}}",
-obj.mobileProfile.relationshipProfile = "[{\"profileGenerationTimestamp\":1651745145759,\"licenseId\":\"\",\"licenseExpiryTimestamp\":6619406400000,\"appEntitlementStatus\":\"SUBSCRIPTION\",\"activationType\":\"NAMED_USER\",\"billingStatus\":\"NORMAL\",\"usedForLegacyProfile\":true,\"licenseExpiryWarningControl\":{\"warningStartTimestamp\":1872675992000,\"warningInterval\":0}}]",
+obj = {
+  "subscriptions" : [
+    {
+      "subscription_name" : "PDFPack",
+      "subscription_state" : "ACTIVE",
+      "biz_source" : "",
+      "billing_term" : null,
+      "sub_ref" : "",
+      "subscription_level" : "Plus"
+    },
+    {
+      "subscription_name" : "Files",
+      "subscription_state" : "ACTIVE",
+      "biz_source" : "",
+      "billing_term" : null,
+      "sub_ref" : "",
+      "subscription_level" : "Plus"
+    }
+  ]
+}
   
 body = JSON.stringify(obj);
 $done({body});
